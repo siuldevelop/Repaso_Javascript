e69fae57befb6f85ec719b4917ea560aea399281
siul23 <luis7arquez@gmail.com>
2026-03-18 20:36:41 -0500

    func-declarive

---
+++ JS + HTML/functions.js
@@ -0,0 +1,28 @@
+// Funcion declarativa
+
+function saludar(params) {
+    let saludo = prompt('INGRESE UN SAlUDO')
+    alert(saludo)
+}
+
+function despedir(params) {
+    document.write('Bye Yall')
+
+}
+
+saludar()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.write('<br>')
